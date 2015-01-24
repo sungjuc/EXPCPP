8e00e8160688f8d85d5a21773e9b8664079dc229
SungJu Cho <scho@scho-ld1.linkedin.biz>
2015-01-23 18:00:02 -0800

    Adding hash_table header tests.

--- scratches/src/graph/utils/hash_table.h
+++ scratches/src/graph/utils/hash_table.h
@@ -12,15 +12,48 @@ namespace graph {
 namespace utils {
 
 struct HashTableHeader {
+	uint8_t* ptr;
 	uint32_t version_number;  // Hash table version information
 	uint32_t num_hash_buckets; // Number of hash buckets
 	uint32_t hash_bucket_size_bytes; // The size of hash buckets in bytes
 
-	HashTableHeader() {
-		explicit HashTableHeader(uint8_t offset) {
-			ptr = Decode4BytesField(version_number, ptr);
-			ptr = Decode4BytesField(version_number, )
-		}
+	explicit HashTableHeader(uint8_t* ptr, uint8_t offset) {
+		this->ptr = ptr;
+		offset = Decode4BytesField(&version_number, offset);
+		offset = Decode4BytesField(&num_hash_buckets, offset);
+		offset = Decode4BytesField(&hash_bucket_size_bytes, offset);
+	}
+
+	HashTableHeader(uint8_t* ptr, uint32_t version_number,
+			uint32_t num_hash_buckets, uint32_t hash_bucket_size_bytes) {
+		this->ptr = ptr;
+		this->version_number = version_number;
+		this->num_hash_buckets = num_hash_buckets;
+		this->hash_bucket_size_bytes = hash_bucket_size_bytes;
+		EncodeHeader();
+	}
+
+	uint8_t EncodeHeader() {
+		uint8_t offset = 0;
+		offset = Encode4BytesField(version_number, offset);
+		offset = Encode4BytesField(num_hash_buckets, offset);
+		offset = Encode4BytesField(hash_bucket_size_bytes, offset);
+		return offset;
+	}
+
+	uint32_t GetHeaderSize() {
+		return sizeof(HashTableHeader);
+	}
+
+private:
+	uint8_t Encode4BytesField(uint32_t field, uint8_t offset) {
+		*reinterpret_cast<uint32_t *>(this->ptr + offset) = field;
+		return offset + sizeof field;
+	}
+
+	uint8_t Decode4BytesField(uint32_t* field_ptr, uint8_t offset) {
+		*field_ptr = *reinterpret_cast<uint32_t *>(this->ptr + offset);
+		return offset + sizeof *field_ptr;
 	}
 };
 
--- scratches/test/graph/utils/hash_table_tests.cpp
+++ scratches/test/graph/utils/hash_table_tests.cpp
@@ -5,6 +5,40 @@
  *      Author: scho
  */
 
+# include "gtest/gtest.h"
+# include "graph/utils/hash_table.h"
+# include <stdio.h>
+
+using namespace graph::utils;
+
+TEST(HashTable, constructor) {
+	// Prepare memory, test data and create header instance.
+	uint8_t* block = reinterpret_cast<uint8_t *>(malloc(1024));
+
+	uint32_t version_number = 100;  // Hash table version information
+    uint32_t num_hash_buckets = 10; // Number of hash buckets
+	uint32_t hash_bucket_size_bytes = 16; // The size of hash buckets in bytes
+
+	HashTableHeader header(block, version_number, num_hash_buckets, hash_bucket_size_bytes);
+
+	// Get the header entries directly from underlying memory and validate them.
+	uint32_t h_version_number = *reinterpret_cast<uint32_t *>(block);
+	uint32_t h_num_hash_buckets = *reinterpret_cast<uint32_t *>(block + sizeof version_number);
+	uint32_t h_hash_bucket_size_bytes = *reinterpret_cast<uint32_t *>(block + sizeof version_number + sizeof num_hash_buckets);
 
 
+	EXPECT_EQ(header.version_number, version_number);
+	EXPECT_EQ(header.num_hash_buckets, num_hash_buckets);
+	EXPECT_EQ(header.hash_bucket_size_bytes, hash_bucket_size_bytes);
 
+	EXPECT_EQ(h_version_number, version_number);
+	EXPECT_EQ(h_num_hash_buckets, num_hash_buckets);
+	EXPECT_EQ(h_hash_bucket_size_bytes, hash_bucket_size_bytes);
+
+	// Create another header using underlying memory to parse.
+	HashTableHeader readHeader(block, 0);
+	EXPECT_EQ(readHeader.version_number, version_number);
+	EXPECT_EQ(readHeader.num_hash_buckets, num_hash_buckets);
+	EXPECT_EQ(readHeader.hash_bucket_size_bytes, hash_bucket_size_bytes);
+
+}
